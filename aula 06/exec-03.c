29a72b641f28d48e2a740326c88568503106f4df
Vinícius Pires <vini.abpires44@gmail.com>
2023-04-26 18:55:25 -0300

    feat: :zap: aula 6 - exercicio 3
    terminei o exercicio

--- aula 06/exec-03.c
+++ aula 06/exec-03.c
@@ -9,7 +9,7 @@
 
 int main() {
   float coeficienteAA, coeficienteBB, coeficienteCC;
-  // float baskara;
+  float baskara;
   float delta;
 
   printf("Insira o coeficiente A:\n");
@@ -21,6 +21,22 @@ int main() {
 
   delta = pow(coeficienteBB, 2);
   delta = delta - 4 * coeficienteAA * coeficienteCC;
+
+  if (delta < 0) {
+    printf("Essa operação não possui raizes reais");
+  } else {
+    delta = sqrt(delta);
+    baskara = (-coeficienteBB + delta) / 2 * coeficienteAA;
+    if (delta == 0) {
+      printf("Essa operação possui somente 1 Raiz de X: %f", baskara);
+    } else {
+      printf("Essa operação possui duas raizes reais:\n");
+      printf("Raiz de X1: %.3f\n", baskara);
+      baskara = (-coeficienteBB - delta) / 2 * coeficienteAA;
+      printf("Raiz de X2: %.3f", baskara);
+    }
+  }
+  printf("\n");
 
   return 0;
 }
