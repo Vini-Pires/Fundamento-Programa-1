363d8f96cebd9faf5af7529d320df56edaa01770
Vinicius UTFPR <viniciuspires@alunos.utfpr.edu.br>
2023-04-27 11:18:29 -0300

    feat(exercicios): :sparkles: exercicios da lista

---
+++ lista-prova/exec-01.c
@@ -0,0 +1,31 @@
+// 1) Sabendo-se que 200 quilowatts de energia custa um quarto do salário mínimo, faça um programa que receba o valor do salário mínimo e a quantidade de quilowatts gasta por uma residência e mostre:
+// a) O valor em reais de cada quilowatt;
+// b) O valor em reais a ser pago pelos quilowatts gasto;
+// c) O novo valor a ser pago por essa residência com um desconto de 12%;
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float quilowattPorReal;
+    float salarioMinimo;
+    float quilowattGasto;
+    float quilowattComDesconto;
+    float contaPagar;
+
+    printf("Defina o salario minimo\n");
+    scanf("%f", &salarioMinimo);
+
+    printf("Quantos quilowatts foram gastos\n");
+    scanf("%f", &quilowattGasto);
+
+    quilowattPorReal = (salarioMinimo / (4 * 200));
+    contaPagar = quilowattPorReal * quilowattGasto;
+    quilowattComDesconto = contaPagar - (contaPagar * (12.0 / 100));
+
+    printf("Cada quilowatt vale R$%.2f\n", quilowattPorReal); // Resposta A
+    printf("Você pagara R$%.2f gastando %.0f quilowatts\n", contaPagar, quilowattGasto); // Resposta B
+    printf("Com desconto você pagara R$%.2f\n", quilowattComDesconto); // Resposta C
+
+    return 0;
+}
---
+++ lista-prova/exec-02.c
@@ -0,0 +1,39 @@
+// 2) Fazer um programa para ler as coordenadas x e y de dois pontos e calcular a distância entre os dois pontos no plano, através da seguinte fórmula:
+// dist = sqrt( (x2-x1)² + (y2 - y1)² )
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+  float coordUmPontoX;
+  float coordUmPontoY;
+  float coordDoisPontoX;
+  float coordDoisPontoY;
+  float distancia;
+  float potenciaDeYs;
+  float potenciaDeXs;
+
+  // Ponto 1 X
+  printf("Insira o X do primeiro ponto\n");
+  scanf("%f", &coordUmPontoX);
+  // Ponto 1 Y
+  printf("Insira o Y do primeiro ponto\n");
+  scanf("%f", &coordUmPontoY);
+  // Ponto 2 X
+  printf("Insira o X do segundo ponto\n");
+  scanf("%f", &coordDoisPontoX);
+  // Ponto 2 Y
+  printf("Insira o Y do segundo ponto\n");
+  scanf("%f", &coordDoisPontoY);
+
+  potenciaDeYs = pow(coordDoisPontoY - coordUmPontoY, 2);
+  potenciaDeXs = pow(coordDoisPontoX - coordUmPontoX, 2);
+
+  distancia = sqrt(potenciaDeXs + potenciaDeYs);
+
+  printf("A distancia entre as coordenadas (%f,%f) ", coordUmPontoX, coordUmPontoY);
+  printf("e as coordenadas (%f,%f) é: %f\n", coordDoisPontoX, coordDoisPontoY, distancia);
+
+  return 0;
+}
